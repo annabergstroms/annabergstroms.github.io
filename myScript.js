3624dcc3fefbd3f30237a5bd3a5578e1eedc8ee9
annabergstroms <73111554+annabergstroms@users.noreply.github.com>
2024-12-10 22:18:32 +0100

    TEST HELT NY

--- myScript.js
+++ myScript.js
@@ -1,9 +1,21 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+/*function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
     } else {
       x.className = "topnav";
     }
-  }
+  }*/
+
+
+
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const hamburger = document.getElementById("hamburger");
+        const navLinks = document.getElementById("nav-links");
+      
+        hamburger.addEventListener("click", function () {
+          navLinks.classList.toggle("active");
+        });
+      });
